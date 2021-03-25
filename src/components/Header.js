bbfa68762e9a7ecf3842400aaa69a736d2776281
trevorcarr <sicktightyo@gmail.com>
2021-03-25 10:47:58 -0700

    Finishing the task tracker app

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,24 @@
-import React from 'react'
+import { useLocation } from 'react-router-dom'
 import Button from './Button'
 
-const Header = () => {
 
-    const onClick = () => {
-        console.log('CLICKs')    
-    }
 
+const Header = ({ title, onAdd, showAdd }) => {
+    const location = useLocation()
+  
     return (
-        <header className='header'>
-            <h1>Task Trackers</h1>
-            <Button color='steelblue' text='Add' onClick={onClick} />
-        </header>
+      <header className='header'>
+        <h1>{title}</h1>
+        {location.pathname === '/' && (
+          <Button
+            color={showAdd ? 'red' : 'green'}
+            text={showAdd ? 'Close' : 'Add'}
+            onClick={onAdd}
+          />
+        )}
+      </header>
     )
-}
+  }
 
 
 export default Header
